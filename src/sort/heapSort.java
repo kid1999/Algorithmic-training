43fdd5cdd431cce511ba0f9ed3ce82a9ae5a45c7
kid1999 <kid.1447250889@live.com>
2019-10-18 19:49:50 +0800

    删除 workspace.xml
    git  rm -r --cached .idea

---
+++ src/sort/heapSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class heapSort {
+  public static void main(String[] args) {
+    int[] arr = {1,5,3,2,7,6,4,9,8};
+    int k = 3;
+    PriorityQueue<Integer> q = new PriorityQueue<>(k);
+    PriorityQueue<Integer> q1 = new PriorityQueue<>(k, new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        return o2-o1;
+      }
+    });
+    for (int i = 0; i <9 ; i++) {
+      if(q.size() < k) q.add(arr[i]);
+      else if(arr[i] > q.peek()){
+        q.poll();
+        q.add(arr[i]);
+      }
+    }
+    for (int i = 0; i <k ; i++) {
+      System.out.println(q.poll());
+    }
+  }
+}

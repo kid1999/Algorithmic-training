eeeb7ab21a854a1f4c2baad2d29c912a271f7a9a
hell Xy <kid.1447250889@live.com>
2020-07-26 09:09:57 +0800

    week198

--- src/LeetCode/P32.java
+++ src/LeetCode/P32.java
@@ -31,19 +31,19 @@ public class P32 {
     }
 
     // 栈
-    public int longestValidParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
-        int max = 0;
-        for (int i = 0; i <s.length() ; i++) {
-            if(s.charAt(i) == ')'){
-                stack.pop();
-                if(stack.isEmpty()) stack.push(i);      // 左边界
-                else max = Math.max(max,i-stack.peek());
-            }else{
-                stack.push(i);
-            }
-        }
-        return max;
-    }
+//    public int longestValidParentheses(String s) {
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(-1);
+//        int max = 0;
+//        for (int i = 0; i <s.length() ; i++) {
+//            if(s.charAt(i) == ')'){
+//                stack.pop();
+//                if(stack.isEmpty()) stack.push(i);      // 左边界
+//                else max = Math.max(max,i-stack.peek());
+//            }else{
+//                stack.push(i);
+//            }
+//        }
+//        return max;
+//    }
 }
